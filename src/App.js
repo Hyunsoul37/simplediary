b91b45b18062d5ade60ca0e8403da0a620d1a1cf
Hyunsoul37 <soulhyun37@gmail.com>
2024-01-12 00:44:50 +0900

    [ADD] diary items
    더미데이터를 이용한 일기 아이템 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "asdf",
+    content: "hihih",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "zxcv",
+    content: "hidddhih",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "fjfififkjf",
+    content: "hihih33333",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 4,
+    author: "asdf12e3",
+    content: "hihih6",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
