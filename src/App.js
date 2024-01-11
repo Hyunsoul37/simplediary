fe94aba0c98e42e2bdc909a441ed0be01192b0e3
Hyunsoul37 <soulhyun37@gmail.com>
2024-01-12 01:15:52 +0900

    [MODIFY] 일기 저장 구현
    기존 더미데이터가 아닌 작성한 데이터로 구동하도록 구현

--- src/App.js
+++ src/App.js
@@ -1,43 +1,62 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 
-const dummyList = [
-  {
-    id: 1,
-    author: "asdf",
-    content: "hihih",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "zxcv",
-    content: "hidddhih",
-    emotion: 4,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "fjfififkjf",
-    content: "hihih33333",
-    emotion: 3,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 4,
-    author: "asdf12e3",
-    content: "hihih6",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-];
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: "asdf",
+//     content: "hihih",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: "zxcv",
+//     content: "hidddhih",
+//     emotion: 4,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: "fjfififkjf",
+//     content: "hihih33333",
+//     emotion: 3,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 4,
+//     author: "asdf12e3",
+//     content: "hihih6",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+// ];
 
 function App() {
+  const [data, setData] = useState([]);
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
