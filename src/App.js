850507b1f345bb17546c1bd7f706ba7b593307b3
Hyunsoul37 <soulhyun37@gmail.com>
2024-01-14 19:25:58 +0900

    [MODIFY] 다이어리 리스트 수정
    수정 및 삭제 기능 추가 구현

--- src/App.js
+++ src/App.js
@@ -53,10 +53,24 @@ function App() {
     setData([newItem, ...data]);
   };
 
+  const onRemove = (targetId) => {
+    console.log(`${targetId}가 삭제되었습니다.`);
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    setData(newDiaryList);
+  };
+
+  const onEdit = (targetId, newContet) => {
+    setData(
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContet } : it
+      )
+    );
+  };
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList diaryList={data} />
+      <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
 }
