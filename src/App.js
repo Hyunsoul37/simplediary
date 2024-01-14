41de27a9c46223b4166bffa6381ec57a794e14ad
Hyunsoul37 <soulhyun37@gmail.com>
2024-01-14 21:35:20 +0900

    [ADD] API 호출 및 init data 세팅

--- src/App.js
+++ src/App.js
@@ -1,43 +1,36 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 import Lifecyle from "./Lifecycle";
 
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: "asdf",
-//     content: "hihih",
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 2,
-//     author: "zxcv",
-//     content: "hidddhih",
-//     emotion: 4,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 3,
-//     author: "fjfififkjf",
-//     content: "hihih33333",
-//     emotion: 3,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 4,
-//     author: "asdf12e3",
-//     content: "hihih6",
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-// ];
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [data, setData] = useState([]);
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -70,7 +63,6 @@ function App() {
 
   return (
     <div className="App">
-      <Lifecyle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
